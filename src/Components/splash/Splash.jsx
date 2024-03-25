c1e259cf0422a505add3eb0b841525b7cb051b9a
Erik Russell <erikrussells@gmail.com>
2024-03-25 16:52:15 -0400

    splash page adjustments

--- src/Components/splash/Splash.jsx
+++ src/Components/splash/Splash.jsx
@@ -3,14 +3,14 @@ import "./splash.css";
 
 function Splash() {
   return (
-    <>
+    <div className="splash-body">
       <SplashHeader />
       <div className="splash-content-div">
         <div className="splash-square-div">
           <div className="splash-square"></div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
