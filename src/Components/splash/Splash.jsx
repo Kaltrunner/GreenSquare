512658faf96b5879f5ec181895b26532f9599c99
Erik Russell <erikrussells@gmail.com>
2024-03-29 20:13:04 -0400

    changed gsap to green square

--- src/Components/splash/Splash.jsx
+++ src/Components/splash/Splash.jsx
@@ -1,7 +1,37 @@
+import { useEffect } from "react";
 import SplashHeader from "../splashheader/SplashHeader";
+import gsap from "gsap";
 import "./splash.css";
 
 function Splash() {
+  useEffect(() => {
+    const splash = document.querySelector(".splash-content-div");
+    const square = splash.querySelectorAll(".splash-square");
+
+    const initHero = () => {
+      gsap.set(square, { opacity: "0" });
+    };
+
+    const showHero = () => {
+      gsap.timeline({ defaults: { ease: "expo-out" } }).fromTo(
+        square,
+        {
+          opacity: 0,
+          webkitFilter: "blur(2px)",
+        },
+        {
+          duration: 2,
+          opacity: 1,
+          x: 0,
+          webkitFilter: "blur(0px)",
+          stagger: 0.015,
+        },
+        0
+      );
+    };
+    initHero();
+    showHero();
+  }, []);
   return (
     <div className="splash-body">
       <SplashHeader />
--- src/Components/splashheader/SplashHeader.jsx
+++ src/Components/splashheader/SplashHeader.jsx
@@ -1,68 +1,8 @@
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
 import arrow from "../../assets/icons8-rightward-up-arrow-100.png";
-import gsap from "gsap";
 import "./splashheader.css";
 
 function SplashHeader() {
-  useEffect(() => {
-    const splash = document.querySelector(".splash-header-div");
-    const splashTitle = splash.querySelectorAll(".splash-header");
-    // const text = splash.querySelectorAll(".splash-p");
-    // const created = splash.querySelectorAll(".portfolio-links");
-
-    const initHero = () => {
-      gsap.set(splashTitle, { x: "-10%" });
-      // gsap.set(text, { x: "-10%" });
-      // gsap.set(created, { x: "10%" });
-    };
-
-    const showHero = () => {
-      gsap.timeline({ defaults: { ease: "expo-out" } }).fromTo(
-        splashTitle,
-        {
-          opacity: 0,
-          webkitFilter: "blur(1px)",
-        },
-        {
-          duration: 1.25,
-          opacity: 1,
-          x: 0,
-          webkitFilter: "blur(0px)",
-          stagger: 0.015,
-        },
-        0
-      );
-      // .fromTo(
-      //   text,
-      //   {
-      //     opacity: 0,
-      //   },
-      //   {
-      //     duration: 1.25,
-      //     opacity: 1,
-      //     x: 0,
-      //     stagger: 0.015,
-      //   },
-      //   0
-      // )
-      // .fromTo(
-      //   created,
-      //   {
-      //     opacity: 0,
-      //   },
-      //   {
-      //     duration: 1.25,
-      //     opacity: 1,
-      //     x: 0,
-      //     stagger: 0.015,
-      //   },
-      //   0
-      // );
-    };
-    initHero();
-    showHero();
-  }, []);
   return (
     <div className="splashheader-content">
       <div className="splash-header-div">
